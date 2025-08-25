a9e5c9d714fea7fcd497387b47481fb49933bc7e
周睿 <zrufo747@outlook.com>
2025-08-25 13:21:17 +0800

    fix: update max packet size handling for USB port speeds

--- usb-host/src/backend/xhci/mod.rs
+++ usb-host/src/backend/xhci/mod.rs
@@ -350,7 +350,7 @@ fn parse_default_max_packet_size_from_port_speed(speed: u8) -> u16 {
     match speed {
         1 => 8,
         2 | 3 => 64,
-        5 | 6 => 512,
+        4 | 5 | 6 => 512,
         v => unimplemented!("PSI: {}", v),
     }
 }
